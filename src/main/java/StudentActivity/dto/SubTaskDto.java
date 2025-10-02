f83c205877588963f0aff366d9fb8d38244a6279
Ayushmaan Singh <rajputayushman119@gmail.com>
2025-10-02 18:21:49 +0530

    sub task dto

--- src/main/java/StudentActivity/dto/SubTaskDto.java
+++ src/main/java/StudentActivity/dto/SubTaskDto.java
@@ -1,6 +1,22 @@
 package StudentActivity.dto;
 
 public class SubTaskDto {
+    private String title;
+    private boolean isComplete;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
 }
