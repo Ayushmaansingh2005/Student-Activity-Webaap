f817baf92236c22bf448178ecfd1de7318e87ee6
Ayushmaan Singh <rajputayushman119@gmail.com>
2025-12-29 22:33:44 +0530

    Spring Security user configurations in the backened

--- src/main/java/StudentActivity/dao/StudentDao.java
+++ src/main/java/StudentActivity/dao/StudentDao.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 @Repository
